d08da19e8d0716add20cf0f81e4db3cf4daa9144
Sylvain Guérin <sylvain.guerin@imt-atlantique.fr>
2026-03-19 09:22:46 +0100

    LOW / Improved assertion support

--- opc-ua-ta-test/src/test/java/org/openflexo/ta/opcua/fml/AutomatedTests.java
+++ opc-ua-ta-test/src/test/java/org/openflexo/ta/opcua/fml/AutomatedTests.java
@@ -39,6 +39,7 @@
 package org.openflexo.ta.opcua.fml;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -56,6 +57,7 @@ import org.openflexo.foundation.fml.cli.CommandInterpreter;
 import org.openflexo.foundation.fml.cli.ParseException;
 import org.openflexo.foundation.fml.cli.command.FMLCommandExecutionException;
 import org.openflexo.foundation.fml.cli.command.FMLScript;
+import org.openflexo.foundation.fml.cli.command.fml.FMLAssertException;
 import org.openflexo.foundation.fml.cli.test.FMLScriptParserTestCase;
 import org.openflexo.pamela.exceptions.ModelDefinitionException;
 import org.openflexo.rm.Resource;
@@ -112,7 +114,11 @@ public class AutomatedTests extends FMLScriptParserTestCase {
 		System.out.println("Parse script " + fmlResource.getRelativePath());
 		script = parseFMLScript(fmlResource, commandInterpreter);
 		checkFMLScript(fmlResource.getRelativePath(), script);
-		script.execute();
+		try {
+			script.execute();
+		} catch (FMLAssertException e) {
+			fail(e.getMessage());
+		}
 	}
 
 	/*	@Test
